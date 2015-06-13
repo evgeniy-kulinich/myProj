4e84d364303f1a13459fb74773b3be1a78e1a954
EvgeniyKulinich <kharkovhipster@gmail.com>
2015-06-13 22:44:49 +0300

    Add Sass

--- js/js.js
+++ js/js.js
@@ -72,36 +72,11 @@ $('.to-top').toTop({
 }( jQuery ));
 
 
-// HAMBURGLERv2
-
-function togglescroll() {
-  $('body').on('touchstart', function(e) {
-    if ($('body').hasClass('noscroll')) {
-      e.preventDefault();
-    }
-  });
-}
-
 $(document).ready(function() {
-  togglescroll()
-  $(".icon").click(function() {
-    $(".mobilenav").fadeToggle(500);
-    $(".top-menu").toggleClass("top-animate");
-    $("body").toggleClass("noscroll");
-    $(".mid-menu").toggleClass("mid-animate");
-    $(".bottom-menu").toggleClass("bottom-animate");
-  });
-});
-
-
-// PUSH ESC KEY TO EXIT
-
-$(document).keydown(function(e) {
-  if (e.keyCode == 27) {
-    $(".mobilenav").fadeOut(500);
-    $(".top-menu").removeClass("top-animate");
-    $("body").removeClass("noscroll");
-    $(".mid-menu").removeClass("mid-animate");
-    $(".bottom-menu").removeClass("bottom-animate");
-  }
+    
+    $('.button-down').click(function(){
+        $('html, body').animate({scrollTop:$(document).height()}, 'slow');
+        return false;
+    });
+ 
 });
